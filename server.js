e5b0a464b39b1dd1de3278b838dfe13d7b19104f
Kristian <kristiankassoemeier@gmail.com>
2023-07-05 19:45:06 +0200

    refactored to company

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const Product = require('./models/productModel')
 const Companies = require('./models/companyModel')
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+const port = 9000
+const connectionString =
+  'mongodb+srv://admin:admin@devtaminapi.xmsqbyi.mongodb.net/?retryWrites=true&w=majority'
 
 //routes
 
@@ -13,29 +16,10 @@ app.get('/', (req, res) => {
   res.send('Heeelloooo. This is an API for my cvr.dk mockup')
 })
 
-app.get('/products', async (req, res) => {
-  try {
-    const products = await Product.find({})
-    res.status(200).json(products)
-  } catch (error) {
-    res.status(500).json({ message: error.message })
-  }
-})
-
 app.get('/companies', async (req, res) => {
   try {
     const companies = await Companies.find({})
     res.status(200).json(companies)
-  } catch (error) {
-    res.status(500).json({ message: error.message })
-  }
-})
-
-app.get('/products/:id', async (req, res) => {
-  try {
-    const { id } = req.params
-    const product = await Product.findById(id)
-    res.status(200).json(product)
   } catch (error) {
     res.status(500).json({ message: error.message })
   }
@@ -61,46 +45,36 @@ app.post('/companies', async (req, res) => {
   }
 })
 
-app.post('/products', async (req, res) => {
-  try {
-    const product = await Product.create(req.body)
-    res.status(200).json(product)
-  } catch (error) {
-    console.log(error.message)
-    res.status(500).json({ message: error.message })
-  }
-})
-
-// update a product
-app.put('/products/:id', async (req, res) => {
+// update a company
+app.put('/companies/:id', async (req, res) => {
   try {
     const { id } = req.params
-    const product = await Product.findByIdAndUpdate(id, req.body)
+    const companies = await Companies.findByIdAndUpdate(id, req.body)
     // we cannot find any product in database
-    if (!product) {
+    if (!companies) {
       return res
         .status(404)
-        .json({ message: `cannot find any product with ID ${id}` })
+        .json({ message: `cannot find any company with ID ${id}` })
     }
-    const updatedProduct = await Product.findById(id)
+    const updatedProduct = await Companies.findById(id)
     res.status(200).json(updatedProduct)
   } catch (error) {
     res.status(500).json({ message: error.message })
   }
 })
 
-// delete a product
+// delete a company
 
-app.delete('/products/:id', async (req, res) => {
+app.delete('/companies/:id', async (req, res) => {
   try {
     const { id } = req.params
-    const product = await Product.findByIdAndDelete(id)
-    if (!product) {
+    const company = await Companies.findByIdAndDelete(id)
+    if (!company) {
       return res
         .status(404)
-        .json({ message: `cannot find any product with ID ${id}` })
+        .json({ message: `cannot find any company with ID ${id}` })
     }
-    res.status(200).json(product)
+    res.status(200).json(company)
   } catch (error) {
     res.status(500).json({ message: error.message })
   }
@@ -108,13 +82,11 @@ app.delete('/products/:id', async (req, res) => {
 
 mongoose.set('strictQuery', false)
 mongoose
-  .connect(
-    'mongodb+srv://admin:admin@devtaminapi.xmsqbyi.mongodb.net/?retryWrites=true&w=majority'
-  )
+  .connect(connectionString)
   .then(() => {
     console.log('connected to MongoDB')
-    app.listen(3000, () => {
-      console.log(`Node API app is running on port 3000`)
+    app.listen(port, () => {
+      console.log(`Node API app is running on port ${port}}`)
     })
   })
   .catch((error) => {
